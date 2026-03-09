5d7f931350bb0d038ab38d42cc307c28053f0488
Wai Yan Bo <shoaiev126.wyb@gmail.com>
2026-03-09 13:57:28 +0800

    Update Page Optimization [skip ci]

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,5 +1,6 @@
-// @ts-check
+// astro.config.mjs
 import { defineConfig } from 'astro/config';
 
-// https://astro.build/config
-export default defineConfig({});
+export default defineConfig({
+  site: 'https://fabareelkreatif.com', // ADD THIS LINE
+});
